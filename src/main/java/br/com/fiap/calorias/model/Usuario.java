5bec43bb244322b4865c6518416bd323aafa869c
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-10-16 22:45:08 -0300

    usuarioRole

--- src/main/java/br/com/fiap/calorias/model/Usuario.java
+++ src/main/java/br/com/fiap/calorias/model/Usuario.java
@@ -2,6 +2,11 @@ package br.com.fiap.calorias.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "tbl_usuarios")
@@ -11,7 +16,7 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue
             (strategy = GenerationType.SEQUENCE,
@@ -23,4 +28,42 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private UsuarioRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
